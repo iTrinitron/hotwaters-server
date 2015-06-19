5dd8038a731df18c7e21eb31a9c5f011628a6b53
Michael Chin <michaelchin1@Michaels-MacBook-Pro.local>
2015-06-19 09:02:09 -0700

    with ac

---
+++ public/config.js
@@ -0,0 +1,13 @@
+/*
+ * Configuration File for Circle Packing
+ */
+
+//https://docs.google.com/spreadsheets/d/1Q5IzubUmGQ213qASClqNxCLotvrryNCEuZjsoOIM0q0/edit#gid=0
+//https://docs.google.com/spreadsheets/d/1Hrke-WfYToPGN3du07Ht4aWU6Ge3yOdOXCEMUOBRoL4/pubhtml
+//https://docs.google.com/spreadsheets/d/{--KEY--}/edit#gid=0
+var key = "1Hrke-WfYToPGN3du07Ht4aWU6Ge3yOdOXCEMUOBRoL4";
+
+var sectionTextSize = 25;
+
+var parentTextSize = 15;
+
