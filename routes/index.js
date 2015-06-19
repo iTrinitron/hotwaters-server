156c86d316c9f99787227c530958abcf12a312b1
Michael Chin <michaelchin1@Michaels-MacBook-Pro.local>
2015-06-19 15:12:43 -0700

    changed labels to no-labels

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('labels.html');
+  res.render('no-labels.html');
 });
 
 module.exports = router;
